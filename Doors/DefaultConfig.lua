3219e9adfd79f16b9d4e533840cce20045c0363f
$Noah <127032836+NoahCodeMaster@users.noreply.github.com>
2024-07-18 14:56:01 +0300

    Update DefaultConfig.lua

--- Doors/DefaultConfig.lua
+++ Doors/DefaultConfig.lua
@@ -1,1 +1,28 @@
-Unknown for now
+return {
+    Speed = 100,
+    DelayTime = 3,
+    HeightOffset = 0,
+    CanKill = true,
+    KillRange = 40,
+    BackwardsMovement = false,
+    BreakLights = true,
+    FlickerLights = {
+        false,
+        3,
+    },
+    Cycles = {
+        Min = 2,
+        Max = 5,
+        WaitTime = 1,
+    },
+    CamShake = {
+        true,
+        {7.2, 18, 1, 1},
+        100,
+    },
+    Jumpscare = {
+        false,
+        {},
+    },
+    CustomDialog = {},
+}
